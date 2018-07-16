b6bcdfda21401a0425426158ac1b7bfcf7078491
dnov09 <24783200+dnov09@users.noreply.github.com>
2018-07-16 12:10:14 -0500

    Solution for CodeSingal - adjacentElementsProduct

---
+++ adjacentElementsProduct.py
@@ -0,0 +1,9 @@
+def adjacentElementsProduct(inputArray):
+    rv = []
+    count = 0
+    while count < (len(inputArray) - 1):
+        rv.append(inputArray[count] * inputArray[count + 1])
+        count += 1
+    return max(rv)
+
+adjacentElementsProduct(inputArray)
